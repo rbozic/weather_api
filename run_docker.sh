982f8c5d89dc15a030c343bc95c46ee034947051
rbozic <68011874+rbozic@users.noreply.github.com>
2023-07-10 12:02:24 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ "$1" == "rain"]]; then
-  docker run --name weather_container weather_image python main.py rain
+  docker run --rm --name weather_container weather_image python main.py rain
 elif [[ "$1" == "shine"]]; then
-  docker run --name weather_container weather_image python main.py shine
+  docker run --rm --name weather_container weather_image python main.py shine
 else
   echo "Invalid option provided."
 fi
